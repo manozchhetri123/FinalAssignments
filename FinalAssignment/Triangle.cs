bc076463aee03c33284ac30abce8b670ae4a3a66
manoz chhetri <manoz.chhetri1@outlook.com>
2020-06-12 14:15:26 +0545

    start of the triangle

--- FinalAssignment/Triangle.cs
+++ FinalAssignment/Triangle.cs
@@ -39,23 +39,5 @@ namespace FinalAssignment
         {
             //_size1, _size2, xi1,yi1,xi2,yi2,xii1,yii1,xii2,yii2,xiii1,yiii1,xiii2,yiii2
             this.texturestyle = texturestyle;
-            this.bb = bb;
-            this.c1 = c1;
-
-            this.xi1 = list[0];
-            this.yi1 = list[1];
-            this.xi2 = list[2];
-            this.yi2 = list[3];
-
-            this.xii1 = list[4];
-            this.yii1 = list[5];
-            this.xii2 = list[6];
-            this.yii2 = list[7];
-
-            this.xiii1 = list[8];
-            this.yiii1 = list[9];
-            this.xiii2 = list[10];
-            this.yiii2 = list[11];
-        }
-    }
+          
 }
