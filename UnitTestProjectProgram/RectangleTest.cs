edcfc0c2981b4ec7011bcf810123c6fab6e52695
manoz chhetri <manoz.chhetri1@outlook.com>
2020-06-16 19:17:59 +0545

    rectangle test begin

--- UnitTestProjectProgram/RectangleTest.cs
+++ UnitTestProjectProgram/RectangleTest.cs
@@ -39,13 +39,6 @@ namespace UnitTestProjectProgram
 
 
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var r = new FinalAssignment.Rectangle();
-            int x = 200, y = 200, size = 100, size1 = 100;
-            r.set(texturestyle, bb, c1, x, y, size, size1);
-            Assert.AreEqual(200, r.x);
-        }
+        
     }
 }
