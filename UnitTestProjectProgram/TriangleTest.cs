d2f7ca1fbee1ebff8935cb7af25cd0143086eedd
manoz chhetri <manoz.chhetri1@outlook.com>
2020-06-16 20:19:02 +0545

    triangle test 1

--- UnitTestProjectProgram/TriangleTest.cs
+++ UnitTestProjectProgram/TriangleTest.cs
@@ -15,12 +15,7 @@ namespace UnitTestProjectProgram
         [TestMethod]
 
 
-        public void TestMethod1()
-        {
-            var t = new FinalAssignment.Triangle();
-            int xi1 = 100, yi1 = 200, xi2 = 200, yi2 = 200, xii1 = 200, yii1 = 200, xii2 = 200, yii2 = 200, xiii1 = 200, yiii1 = 200, xiii2 = 200, yiii2 = 200;
-            t.set(texturestyle, bb, c1, xi1, yi1, xi2, yi2, xii1, yii1);
-            Assert.AreEqual(100, t.xi1);
+      
         }
     }
 }
